48f3f787206d285d02adcef776d06a6a56f5ad03
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2021-11-17 16:52:56 +0530

    Update display_progress.py

--- bot/plugins/display/display_progress.py
+++ bot/plugins/display/display_progress.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-# This is bot coded by AbhijithNT and used for educational purposes only
-# https://github.com/AbhijithNT
-# (c) Abhijith N T ;-)
+# This is bot coded by Judson-web and used for educational purposes only
+# https://github.com/Judson-web
+# (c) Judson-web ;-)
 # Thank you https://github.com/pyrogram/pyrogram :-)
 
 async def progress(current, total, up_msg, message):
     try:
         await message.edit(
-            text=f"{up_msg} {current * 100 / total:.1f}%"
+            text=f"{up_msg} {ᴄᴜʀʀᴇɴᴛ * 100 / ᴛᴏᴛᴀʟ:.1f}%"
         )
     except:
         pass
